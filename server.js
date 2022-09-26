d9e69a55bacb9721f50feb19783f047378d67a10
vinicius moura <viniciusmirandamoura5@gmail.com>
2022-09-26 20:32:39 -0300

    Incluida a mensagem que o cliente conectou

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.static("public"))
 const http = require("http").Server(app)
 const PORT = process.env.PORT || 8000
 
-                  //FUNÇÃO DE CALLBACK
+//FUNÇÃO DE CALLBACK
 http.listen(PORT, () => console.log(`Servidor iniciado na porta ${PORT}`))
 
 app.get("/",(_, response) => response.sendFile(`${__dirname}/index.html`))
@@ -14,11 +14,12 @@ app.get("/",(_, response) => response.sendFile(`${__dirname}/index.html`))
 const serverSocket = require("socket.io")(http)
 serverSocket.on("connect", socket =>{
     console.log(`Cliente ${socket.id} conectou`)
-    socket.on("chat msg",msg =>serverSocket.emit("chat msg",`${socket.username}: ${msg}`))
+    socket.on("chat msg", msg => serverSocket.emit("chat msg",`${socket.username}: ${msg}`))
 
-    socket.on("login",username=>{
-    socket.username=username
-    serverSocket.emit("chat msg",msg)
+    socket.on("login", username => {
+        socket.username=username
+        const msg = `Usuario ${username} conectou`
+        serverSocket.emit("chat msg", msg)
     })
 
 })
